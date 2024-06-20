89fc62ee10969a36d9ae76ae9ca0a018a2d51a1a
GreyHunt3r <svenimholz@icloud.com>
2024-06-20 16:32:50 +0200

    Added Authentication scaffolding and Games page

--- MinimalSetGame.Client/Program.cs
+++ MinimalSetGame.Client/Program.cs
@@ -1,3 +1,5 @@
+using BlazorWasmAuth.Identity;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MinimalSetGame.Client.Components;
@@ -6,7 +8,35 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// register the cookie handler
+builder.Services.AddTransient<CookieHandler>();
+
+// set up authorization
+builder.Services.AddAuthorizationCore();
+
+// register the custom state provider
+builder.Services
+    .AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+
+// register the account management interface
 builder.Services.AddScoped(
-sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
+
+// set base address for default host
+builder.Services.AddScoped(
+sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri(
+        builder.Configuration["FrontendUrl"] ?? "https://localhost:5002")
+    });
+
+// configure client for auth interactions
+builder.Services.AddHttpClient(
+    "Auth",
+    opt => opt.BaseAddress =
+        new Uri(builder.Configuration["BackendUrl"] ?? "https://localhost:5001"))
+    .AddHttpMessageHandler<CookieHandler>();
+
 
 await builder.Build().RunAsync();
