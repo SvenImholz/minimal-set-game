5c4fddb508d1f97ca98a48cc97f187fd13af6cff
GreyHunt3r <svenimholz@icloud.com>
2024-06-29 16:42:16 +0200

    Added Tests to check the hint functionality

--- MinimalSetGame.Tests/GetHint.cs
+++ MinimalSetGame.Tests/GetHint.cs
@@ -11,12 +11,12 @@ public class GetHint
     public void Setup() {}
 
     [Test]
-    public void GetHint_ReturnsHint_WhenHintIsAvailable()
+    public void GetHint_ReturnsHint_WhenThereIsOnePossibleSet()
     {
         var gameId = new Guid();
         var cards = new List<Card>
         {
-            new Card
+            new Card // This card is part of a set
             {
                 GameId = gameId,
                 Color = Color.Red,
@@ -25,8 +25,51 @@ public class GetHint
                 Shape = Shape.Diamond,
                 IsDrawn = true
             },
-
-          // todo add more cards
+            new Card // This card is part of a set
+            {
+                GameId = gameId,
+                Color = Color.Green,
+                Fill = Fill.Solid,
+                Number = Number.Two,
+                Shape = Shape.Squiggle,
+                IsDrawn = true
+            },
+            new Card
+            {
+                GameId = gameId,
+                Color = Color.Green,
+                Fill = Fill.Solid,
+                Number = Number.Two,
+                Shape = Shape.Squiggle,
+                IsDrawn = true
+            },
+            new Card
+            {
+                GameId = gameId,
+                Color = Color.Red,
+                Fill = Fill.Striped,
+                Number = Number.Three,
+                Shape = Shape.Diamond,
+                IsDrawn = true
+            },
+            new Card
+            {
+                GameId = gameId,
+                Color = Color.Purple,
+                Fill = Fill.Solid,
+                Number = Number.One,
+                Shape = Shape.Oval,
+                IsDrawn = true
+            },
+            new Card // This card is part of a set
+            {
+                GameId = gameId,
+                Color = Color.Purple,
+                Fill = Fill.Striped,
+                Number = Number.Three,
+                Shape = Shape.Oval,
+                IsDrawn = true
+            },
         };
         var game = new Game(gameId)
         {
@@ -36,5 +79,81 @@ public class GetHint
         var result = GameService.GetHint(game);
 
         Assert.That(result, Is.Not.Null);
+    }
+
+    [Test]
+    public void GetHint_ReturnsNull_WhenThePossibleCardsAreAlreadyASetOrNotASet()
+    {
+        var gameId = new Guid();
+        var cards = new List<Card>
+        {
+            new Card // Already in a Set
+            {
+                GameId = new Guid(),
+                Color = Color.Red,
+                Fill = Fill.Outlined,
+                Number = Number.One,
+                Shape = Shape.Diamond,
+                IsDrawn = true
+            },
+            new Card // Already in a Set
+            {
+                GameId = new Guid(),
+                Color = Color.Green,
+                Fill = Fill.Solid,
+                Number = Number.Two,
+                Shape = Shape.Squiggle,
+                IsDrawn = true
+            },
+            new Card //Already in a Set
+            {
+                GameId = new Guid(),
+                Color = Color.Purple,
+                Fill = Fill.Striped,
+                Number = Number.Three,
+                Shape = Shape.Oval,
+                IsDrawn = true
+            },
+            new Card // Not a Set with the remaining cards
+            {
+                GameId = new Guid(),
+                Color = Color.Green,
+                Fill = Fill.Solid,
+                Number = Number.Two,
+                Shape = Shape.Squiggle,
+                IsDrawn = true
+            },
+            new Card // Not a Set with the remaining cards
+            {
+                GameId = new Guid(),
+                Color = Color.Red,
+                Fill = Fill.Striped,
+                Number = Number.Three,
+                Shape = Shape.Diamond,
+                IsDrawn = true
+            },
+            new Card // Not a Set with the remaining cards
+            {
+                GameId = new Guid(),
+                Color = Color.Purple,
+                Fill = Fill.Solid,
+                Number = Number.One,
+                Shape = Shape.Oval,
+                IsDrawn = true
+            },
+
+        };
+
+        var set = new Set(gameId, [cards[0], cards[1], cards[2]]);
+
+        var game = new Game(gameId)
+        {
+            Deck = cards,
+            Sets = [set]
+        };
+
+        var result = GameService.GetHint(game);
+
+        Assert.That(result, Is.Null);
     }
 }
