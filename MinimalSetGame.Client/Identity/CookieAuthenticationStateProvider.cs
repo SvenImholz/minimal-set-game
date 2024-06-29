68f8ccffd1dd33f741a69c9f3bd01f56c4b12b92
GreyHunt3r <svenimholz@icloud.com>
2024-06-29 16:46:27 +0200

    Removed commented code about roles for Player/IdentityUser

--- MinimalSetGame.Client/Identity/CookieAuthenticationStateProvider.cs
+++ MinimalSetGame.Client/Identity/CookieAuthenticationStateProvider.cs
@@ -199,38 +199,6 @@ namespace MinimalSetGame.Client.Identity
                         c => c.Key != ClaimTypes.Name && c.Key != ClaimTypes.Email)
                         .Select(c => new Claim(c.Key, c.Value)));
 
-                    // // tap the roles endpoint for the user's roles
-                    // var rolesResponse = await _httpClient.GetAsync("roles");
-                    //
-                    // // throw if request fails
-                    // rolesResponse.EnsureSuccessStatusCode();
-                    //
-                    // // read the response into a string
-                    // var rolesJson = await rolesResponse.Content.ReadAsStringAsync();
-                    //
-                    // // deserialize the roles string into an array
-                    // var roles = JsonSerializer.Deserialize<RoleClaim[]>(
-                    // rolesJson,
-                    // _jsonSerializerOptions);
-                    //
-                    // // if there are roles, add them to the claims collection
-                    // if (roles?.Length > 0)
-                    // {
-                    //     foreach (var role in roles)
-                    //     {
-                    //         if (!string.IsNullOrEmpty(role.Type) &&
-                    //             !string.IsNullOrEmpty(role.Value))
-                    //         {
-                    //             claims.Add(
-                    //             new Claim(
-                    //             role.Type,
-                    //             role.Value,
-                    //             role.ValueType,
-                    //             role.Issuer,
-                    //             role.OriginalIssuer));
-                    //         }
-                    //     }
-                    // }
 
                     // set the principal
                     var id = new ClaimsIdentity(
